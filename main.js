9ac5f4677f5d228552e09fbfd18bae94d2dceb59
anubittech <anubit.dev@gmail.com>
2024-07-30 17:30:59 +0530

    dgdg

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
-import * as faceAI from 'face-api.js'
+import * as faceAI from "face-api.js";
 
-let Video = document.querySelector('#video')
-let canvas = document.querySelector('canvas')
+let Video = document.querySelector("#video");
+let canvas = document.querySelector("canvas");
 
-function StartVideo (){
- navigator.mediaDevices.getUserMedia({
-  video:{
-    width:1980,
-    height:1080,
-    facingMode:"user"
-  }
- }
- 
-).then((stream)=>{
-  Video.srcObject = stream;
-  Video.play()
-})
-.catch((err)=>{
-  console.log(`Error:${err}`)
-})
- 
+function StartVideo() {
+  navigator.mediaDevices
+    .getUserMedia({
+      video: {
+        width: 1980,
+        // height:1080,
+        facingMode: "user",
+      },
+    })
+    .then((stream) => {
+      Video.srcObject = stream;
+      Video.play();
+    })
+    .catch((err) => {
+      console.log(`Error:${err}`);
+    });
 }
 
-StartVideo()
+StartVideo();
