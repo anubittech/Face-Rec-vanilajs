9cf981bdd97347b20bcdf9e7c7fb9f39a069885f
anubittech <anubit.dev@gmail.com>
2024-08-08 16:14:39 +0530

    yes

--- main.js
+++ main.js
@@ -2,34 +2,32 @@ import * as faceapi from "face-api.js";
 let video = document.querySelector("video");
 let canvas = document.querySelector("canvas");
 
-document.addEventListener("load", () => {
-  async function face() {
-    const MODEL_URL = "/models";
+async function face() {
+  const MODEL_URL = "/models";
 
-    await faceapi.loadSsdMobilenetv1Model(MODEL_URL),
-      await faceapi.loadFaceLandmarkModel(MODEL_URL),
-      await faceapi.loadFaceRecognitionModel(MODEL_URL),
-      await faceapi.loadFaceExpressionModel(MODEL_URL);
-    navigator.mediaDevices
-      .getUserMedia({
-        video: {},
-      })
-      .then((strm) => (video.srcObject = strm))
-      .catch((err) => console.log(err));
+  await faceapi.loadSsdMobilenetv1Model(MODEL_URL),
+    await faceapi.loadFaceLandmarkModel(MODEL_URL),
+    await faceapi.loadFaceRecognitionModel(MODEL_URL),
+    await faceapi.loadFaceExpressionModel(MODEL_URL);
+  navigator.mediaDevices
+    .getUserMedia({
+      video: {},
+    })
+    .then((strm) => (video.srcObject = strm))
+    .catch((err) => console.log(err));
 
-    let faceDetect = await faceapi
-      .detectAllFaces(video)
-      .withFaceLandmarks()
-      .withFaceDescriptors()
-      .withFaceExpressions();
-    let canvasctx = canvas
-      .getContext("2d")
-      .clearRect(0, 0, video.width, video.height);
-    faceapi.matchDimensions(canvasctx, video);
-    let faceResize = faceapi.resizeResults(faceDetect, video);
-    faceapi.draw.drawDetections(canvasctx, faceResize);
-    faceapi.draw.drawFaceLandmarks(canvasctx, faceResize);
-    faceapi.draw.drawFaceExpressions(canvasctx, faceResize);
-  }
-  face();
-});
+  let faceDetect = await faceapi
+    .detectAllFaces(video)
+    .withFaceLandmarks()
+    .withFaceDescriptors()
+    .withFaceExpressions();
+  let canvasctx = canvas
+    .getContext("2d")
+    .clearRect(0, 0, video.width, video.height);
+  faceapi.matchDimensions(canvasctx, video);
+  let faceResize = faceapi.resizeResults(faceDetect, video);
+  faceapi.draw.drawDetections(canvasctx, faceResize);
+  faceapi.draw.drawFaceLandmarks(canvasctx, faceResize);
+  faceapi.draw.drawFaceExpressions(canvasctx, faceResize);
+}
+face();
